32d23eb026cb11f4859e63568a4f29b8f3100fe1
Prihzinha <priscilaraguiar@gmail.com>
2024-03-11 09:45:10 -0300

    atualização

--- 3-Teste-Avançado-com-Pytest-(Desafio 5)/test_advanced.py
+++ 3-Teste-Avançado-com-Pytest-(Desafio 5)/test_advanced.py
@@ -1,20 +1,24 @@
-import pytest 
+import pytest
 
 import os
 
+
 def str_to_bool(string):
-    if string.lower() in ['yes', 'y', '1']:
+    if string.lower() in ["yes", "y", "1"]:
         return True
-    elif string.lower() in ['no', 'n', '0']:
+    elif string.lower() in ["no", "n", "0"]:
         return False
 
-@pytest.mark.parametrize("string", ['Y', 'y', '1', 'YES'])
+
+@pytest.mark.parametrize("string", ["Y", "y", "1", "YES"])
 def test_str_to_bool_true(string):
     assert str_to_bool(string) is True
 
-@pytest.mark.parametrize("string", ['N', 'n', '0', 'NO'])
+
+@pytest.mark.parametrize("string", ["N", "n", "0", "NO"])
 def test_str_to_bool_false(string):
     assert str_to_bool(string) is False
+
 
 @pytest.fixture
 def tmpfile(tmpdir):
@@ -23,12 +27,12 @@ def tmpfile(tmpdir):
         file.write("1")
         return file.strpath
     return write
-    
+
 
 class TestFile:
 
     def setup(self):
-        with open("/tmp/done", 'w') as _f:
+        with open("/tmp/done", "w") as _f:
             _f.write("1")
 
     def teardown(self):
@@ -41,7 +45,7 @@ class TestFile:
         with open("/tmp/done") as _f:
             contents = _f.read()
         assert contents == "1"
-        
+
     def test_f(self, tmpfile):
         path = tmpfile()
         with open(path) as _f:
