5eb42367b91e5345a455fd4cfaf3972e032feb3c
Prihzinha <priscilaraguiar@gmail.com>
2024-03-11 18:26:57 -0300

    atualização

--- 3-Teste-Avançado-com-Pytest-(Desafio 5)/test_advanced.py
+++ 3-Teste-Avançado-com-Pytest-(Desafio 5)/test_advanced.py
@@ -21,3 +21,25 @@ def tmpfile(tmpdir):
         file.write("1")
         return file.strpath
     return write
+
+import os
+
+
+class TestFile:
+
+    def test_f(self, tmpfile):
+        path = tmpfile()
+        with open(path) as _f:
+            contents = _f.read()
+        assert contents == "1"
+
+    def teardown(self):
+        try:
+            os.remove("/tmp/done")
+        except OSError:
+            pass
+
+    def test_done_file(self):
+        with open("/tmp/done") as _f:
+            contents = _f.read()
+        assert contents == "1"
