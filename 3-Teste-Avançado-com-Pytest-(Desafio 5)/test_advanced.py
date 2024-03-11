65d4e445f55116d395be3e63f12a87ff86e07dd5
Prihzinha <priscilaraguiar@gmail.com>
2024-03-11 11:41:53 -0300

    atualização

--- 3-Teste-Avançado-com-Pytest-(Desafio 5)/test_advanced.py
+++ 3-Teste-Avançado-com-Pytest-(Desafio 5)/test_advanced.py
@@ -13,3 +13,37 @@ def test_str_to_bool_true(string):
 @pytest.mark.parametrize("string", ['N', 'n', '0', 'NO'])
 def test_str_to_bool_false(string):
     assert str_to_bool(string) is False
+    
+@pytest.fixture
+def tmpfile(tmpdir):
+    def write():
+        file = tmpdir.join("done")
+        file.write("1")
+        return file.strpath
+    return write
+    
+import os
+
+
+class TestFile:
+
+    def setup(self):
+        with open("/tmp/done", 'w') as _f:
+            _f.write("1")
+
+    def teardown(self):
+        try:
+            os.remove("/tmp/done")
+        except OSError:
+            pass
+
+    def test_done_file(self):
+        with open("/tmp/done") as _f:
+            contents = _f.read()
+        assert contents == "1"
+        
+    def test_f(self, tmpfile):
+        path = tmpfile()
+        with open(path) as _f:
+            contents = _f.read()
+        assert contents == "1"
